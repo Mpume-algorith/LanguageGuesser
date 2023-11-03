51fb00f7d3c6b0a9aec8a14edc23a2500dd64a2b
Mpume-algorith <mpumelelotsotsa@gmail.com>
2023-11-03 03:32:58 +0200

    More comprehensive output

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const langCode = franc(langInput);
 
 if (langCode !== 'und'){
     const indexNumber = langs.codes('3').indexOf(langCode);
-    console.log(langs.names()[indexNumber]);
+    console.log(`Our best guess is: ${langs.names()[indexNumber]}`);
 }
 else {
         console.log(`Please provide longer input`);
